853cd63f405316bb753967d732474c6a0c3f4ed9
Eli Lisseck <lisseck@gmail.com>
2020-10-15 18:02:52 -0700

    initial commit

---
+++ js/formstyles.js
@@ -0,0 +1,3 @@
+CRM.$(function($) {
+  $('#logo').attr('src', '/sites/default/files/civicrm/com.wannapixel.ybstyles/img/yb_orange_blue.png');
+});
---
+++ ybstyles.php
@@ -0,0 +1,179 @@
+<?php
+
+require_once 'ybstyles.civix.php';
+// phpcs:disable
+use CRM_Ybstyles_ExtensionUtil as E;
+// phpcs:enable
+
+function ybstyles_civicrm_buildForm($formName, &$form) {
+  if (($formName == 'CRM_Contribute_Form_Contribution_Main' || $formName == 'CRM_Contribute_Form_Contribution_Confirm' || $formName == 'CRM_Contribute_Form_Contribution_ThankYou') && $form->getVar('_id') == 2) {
+    CRM_Core_Resources::singleton()->addStyleFile('com.wannapixel.ybstyles', 'css/formstyles.css');
+    //CRM_Core_Resources::singleton()->addScriptFile('com.wannapixel.ybstyles', 'js/formstyles.js');
+  }
+}
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function ybstyles_civicrm_config(&$config) {
+  _ybstyles_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function ybstyles_civicrm_xmlMenu(&$files) {
+  _ybstyles_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function ybstyles_civicrm_install() {
+  _ybstyles_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function ybstyles_civicrm_postInstall() {
+  _ybstyles_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function ybstyles_civicrm_uninstall() {
+  _ybstyles_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function ybstyles_civicrm_enable() {
+  _ybstyles_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function ybstyles_civicrm_disable() {
+  _ybstyles_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function ybstyles_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _ybstyles_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function ybstyles_civicrm_managed(&$entities) {
+  _ybstyles_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function ybstyles_civicrm_caseTypes(&$caseTypes) {
+  _ybstyles_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function ybstyles_civicrm_angularModules(&$angularModules) {
+  _ybstyles_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function ybstyles_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _ybstyles_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function ybstyles_civicrm_entityTypes(&$entityTypes) {
+  _ybstyles_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_thems().
+ */
+function ybstyles_civicrm_themes(&$themes) {
+  _ybstyles_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function ybstyles_civicrm_preProcess($formName, &$form) {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function ybstyles_civicrm_navigationMenu(&$menu) {
+//  _ybstyles_civix_insert_navigation_menu($menu, 'Mailings', array(
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ));
+//  _ybstyles_civix_navigationMenu($menu);
+//}
