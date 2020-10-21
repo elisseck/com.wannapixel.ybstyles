1185b04ee29677e941ac7626a9baf72fdd802201
Eli Lisseck <eli@wapix.co>
2020-10-21 21:28:41 +0000

    second round changes

--- js/formstyles.js
+++ js/formstyles.js
@@ -1,3 +1,3 @@
 CRM.$(function($) {
-  $('#logo').attr('src', '/sites/default/files/civicrm/ext/com.wannapixel.ybstyles/img/yb_orange_blue.png');
+  $('#logo').attr('src', '/sites/default/files/civicrm/ext/com.wannapixel.ybstyles/img/yb_orange_blue_2.png');
 });
